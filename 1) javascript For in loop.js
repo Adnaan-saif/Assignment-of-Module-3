efb5c09fddd17d8146bdd2a0b5449189bfbdb3d7
Adnaan Saif <ad.xiii.ad@gmail.com>
2023-08-13 02:15:07 +0600

    taskOneCompleted

---
+++ 1) javascript For in loop.js
@@ -0,0 +1,13 @@
+function printObjectProperties(obj) {
+    if (Object.keys(obj).length === 0) {
+        console.log("Object is empty");
+    } else {
+        for (const items in obj) {
+            if(obj.hasOwnProperty(items)) {
+                console.log(items + ": " + obj[items]);
+            }
+        }
+    }
+}
+
+printObjectProperties();
